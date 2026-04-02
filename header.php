9e1e8b266b6e99fccc7b5e8ea944d3b8d51fa5ce
Kareena2070 <kareenayadav1088@gmail.com>
2026-04-02 16:41:13 +0530

    setup folder stucture

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kareena Site</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+</head>
+<body>
+
+<header>
+    <h2>Kareena Astrology</h2>
+    <nav>
+    <a href="/">Home</a> |
+    <a href="/about">About</a> |
+    <a href="/contact">Contact</a>
+</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <h1>🚀 Kareena Astrology UI</h1>
+    <p>This is my custom WordPress frontend</p>
+</div>
+
+<?php get_footer(); ?>
